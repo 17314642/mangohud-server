cc3283560e9bdb88a1358e96967c8cdf7da9bc14
17314642 <48242771@protonmail.ch>
2025-06-29 13:04:23 +0300

    add ends_with() string helper

--- common/helpers.cpp
+++ common/helpers.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <algorithm>
 #include "helpers.hpp"
 
 std::string read_line(const std::string& filename)
@@ -11,4 +12,17 @@ std::string read_line(const std::string& filename)
 
     std::getline(file, line);
     return line;
+}
+
+bool ends_with(std::string s1, std::string s2, bool ignore_case) {
+    if (s1.size() < s2.size())
+        return false;
+
+    if (ignore_case) {
+        std::transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+        std::transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+    }
+
+    size_t pos = s1.size() - s2.size();
+    return (s1.rfind(s2, pos) == pos);
 }
--- common/helpers.hpp
+++ common/helpers.hpp
@@ -3,3 +3,4 @@
 #include <string>
 
 std::string read_line(const std::string& filename);
+bool ends_with(std::string s1, std::string s2, bool ignore_case = false);
