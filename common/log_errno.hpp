20f34afa93c894f8a462f04b20cf281dff07035c
17314642 <48242771@protonmail.ch>
2025-07-06 17:51:51 +0300

    log_errno: print correct filename and line number
    LOG_UNIX_ERRNO_WARN("some_message")
    
    before it was like this:
    [current_date] [lvl] [log_errno.hpp:28] some_message
    
    now it's:
    [current_date] [lvl] [main.cpp:80] some_message

--- common/log_errno.hpp
+++ common/log_errno.hpp
@@ -3,31 +3,25 @@
 #include <string>
 #include <spdlog/spdlog.h>
 
-#define LOG_UNIX_ERRNO_DEBUG(...) log_unix_errno(log_unix_errno_lvl::debug, __VA_ARGS__)
-#define LOG_UNIX_ERRNO_WARN(...)  log_unix_errno(log_unix_errno_lvl::warn , __VA_ARGS__)
-#define LOG_UNIX_ERRNO_ERROR(...) log_unix_errno(log_unix_errno_lvl::error, __VA_ARGS__)
-
-enum log_unix_errno_lvl {
-    debug = 0,
-    warn,
-    error
-};
+#define LOG_UNIX_ERRNO_GENERIC(lvl, ...) log_unix_errno(lvl, __FILE__, __LINE__, SPDLOG_FUNCTION, __VA_ARGS__)
+#define LOG_UNIX_ERRNO_DEBUG(...) LOG_UNIX_ERRNO_GENERIC(spdlog::level::debug, __VA_ARGS__)
+#define LOG_UNIX_ERRNO_WARN(...)  LOG_UNIX_ERRNO_GENERIC(spdlog::level::warn , __VA_ARGS__)
+#define LOG_UNIX_ERRNO_ERROR(...) LOG_UNIX_ERRNO_GENERIC(spdlog::level::err, __VA_ARGS__)
 
 template <class... Args>
-void log_unix_errno(log_unix_errno_lvl lvl, std::string fmt, Args&&... a) {
+void log_unix_errno(
+    spdlog::level::level_enum lvl,
+    const char* filename_in, int line_in, const char* funcname_in,
+    std::string fmt, Args&&... a
+) {
     fmt += " Error code: {} ({})";
-    switch (lvl) {
-        case debug:
-            SPDLOG_DEBUG(fmt, std::forward<Args>(a)..., errno, strerror(errno));
-            break;
 
-        case warn:
-            SPDLOG_WARN(fmt, std::forward<Args>(a)..., errno, strerror(errno));
-            break;
-
-        case error:
-            SPDLOG_ERROR(fmt, std::forward<Args>(a)..., errno, strerror(errno));
-            break;
-
-    }
+    spdlog::default_logger_raw()->log(
+        spdlog::source_loc{filename_in, line_in, funcname_in},
+        lvl,
+        fmt,
+        std::forward<Args>(a)...,
+        errno,
+        strerror(errno)
+    );
 }
